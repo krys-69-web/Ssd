21b4e4ca8923a17a1084c5a04681cbaa04bfccaa
krys-69-web <yjoanchris225@gmail.com>
2026-09-20 17:22:05 +0000

    test: update app name expectation
    Update the expected app name string in tests to match the current product branding "Infinite Stretch".

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -27,7 +27,7 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("SLM Stretch", appName)
+    assertEquals("Infinite Stretch", appName)
   }
 
   @Test
